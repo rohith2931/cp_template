60fc09dc8ce1fb1450d65a25085e52beac06087a
rohith <akularohith89401@gmail.com>
2021-06-25 13:36:19 +0530

    latest template

--- template.cpp
+++ template.cpp
@@ -1,19 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-#define for(i,a,b) for (int i = a;i < b;i++)
-typedef long long ll;
+#define ll long long
+#define for(i,a,b) for(ll i = a;i < b;i++)
+#define M 1000000007
+#define pb push_back
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
 
 void solve()
 {
-
+    
 }
 int main()
 {
-    int t;
+    int t=1;
     cin>>t;
-    while(t--)
+    for(i,1,t+1)
     {
+        // cout<<"Case #"<<i<<": "<<endl;
         solve();
     }
 }
