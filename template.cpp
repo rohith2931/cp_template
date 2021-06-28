2f30c17c7c62a1ea8776afaf0786acc725362b90
rohith <akularohith89401@gmail.com>
2021-06-28 16:04:40 +0530

    Final Commit

--- template.cpp
+++ template.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define nl "\n"
 #define ll long long
 #define M 1000000007
@@ -13,15 +14,21 @@ typedef vector<int> VI;
 typedef vector<string> VS;
 typedef vector<PII> VII;
 typedef vector<VI> VVI;
-
+void Rohith()
+{
+    FAST
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
 void solve()
 {
     
 }
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    Rohith();
     int t=1;
     cin>>t;
     for(int i=1;i<=t;i++)
